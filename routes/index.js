a84ea27f92c965338a7eca0c313b864f57f4642f
minhdat6312 <minhdat6321@gmail.com>
2024-09-07 15:00:06 -0700

    Setup 1 project Express Js & MongoDB

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const { sendResponse, AppError } = require("../helpers/utils.js")
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.status(200).send("Welcome to Server =]]!")
+});
+
+router.get("/template/:test", async (req, res, next) => {
+  const { test } = req.params
+  try {
+    //turn on to test error handling
+    if (test === "error") {
+      throw new AppError(401, "Access denied", "Authentication Error")
+    } else {
+      sendResponse(res, 200, true, { data: "template" }, null, "template success")
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+//Add foo api to index routes
+const fooRouter = require("./foo.api.js")
+router.use("/foo", fooRouter)
+
+module.exports = router;
